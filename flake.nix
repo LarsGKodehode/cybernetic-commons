b00fd9bef72581d29937a339c9ed20a2e5b22be0
LarsGKodehode <104063134+LarsGKodehode@users.noreply.github.com>
2025-03-22 20:42:25 +0000

    define development environment

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "A cybernetic cooperative experiment.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      withSystem = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+      withPkgs =
+        callback:
+        withSystem (
+          system:
+          callback (
+            import nixpkgs {
+              inherit system;
+            }
+          )
+        );
+    in
+    {
+      devShells = withPkgs (pkgs: {
+        default = pkgs.mkShell {
+          # Project Dependencies
+          packages = [ ];
+
+          # Development Environment Variables
+          env = { };
+        };
+      });
+
+      formatter = withPkgs (pkgs: pkgs.nixfmt-rfc-style);
+    };
+}
